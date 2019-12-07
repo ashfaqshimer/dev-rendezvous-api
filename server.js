7f85766e592312a6fe2827620bf1736bd862a99e
ashfaqshimer <shimer.ahamed@gmail.com>
2019-12-08 00:48:55 +0530

    Develop route functionality and error handling

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDb = require('./config/db');
+const colors = require('colors');
+const errorHandler = require('./middleware/errorHandler');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDb();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -15,14 +21,28 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
 
+// Body parser
+app.use(express.json());
+
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+// Error handler
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+			.bold
 	)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+	console.error(`Error: ${err.message}`.red);
+	// Close server and exit process
+	server.close(() => process.exit(1));
+});
