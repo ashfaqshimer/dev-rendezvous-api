0f05fa939a49cf0979450b0bed91e40566bb0cac
Shimer <ashfaq.shimer@itelasoft.com>
2019-12-16 16:27:52 +0530

    Develop update user functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,6 +42,42 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc    Update user details
+// @route   PUT /api/v1/auth/updatedetails
+// @access  Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const { name, email } = req.body;
+
+  const user = await User.findByIdAndUpdate(
+    req.user.id,
+    { name, email },
+    { new: true, runValidators: true }
+  );
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  res.status(200).json({ success: true, data: user });
+});
+
+// @desc    Update password
+// @route   PUT /api/v1/auth/updatepassword
+// @access  Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  // Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Password is incorrect', 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+  
+  sendTokenResponse(user, 200, res);
+});
+
 // @desc    Get current logged in user
 // @route   POST /api/v1/auth/me
 // @access  Private
