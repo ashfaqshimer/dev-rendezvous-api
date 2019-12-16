f16a84ab9fd6c1b4d401491b3f26037209025e1d
Shimer <ashfaq.shimer@itelasoft.com>
2019-12-16 18:06:12 +0530

    Complete review model

--- models/Review.js
+++ models/Review.js
@@ -17,6 +17,16 @@ const ReviewSchema = new mongoose.Schema(
       min: 1,
       max: 10,
       required: [true, 'Please add a rating  between 1 and 10']
+    },
+    bootcamp: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Bootcamp',
+      required: true
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true
     }
   },
   { timestamps: true }
